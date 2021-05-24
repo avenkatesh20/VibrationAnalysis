a1c204ddef8bef85ebd3db4e3c85f876d7a39749
avenkatesh20 <avenkate@caltech.edu>
2021-05-24 02:50:59 -0700

    Figured out how to store acceleration data in a text file

---
+++ PhidgetCode/phidget_acceleration_acq.c
@@ -0,0 +1,63 @@
+#include <phidget22.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+FILE *PHIDGETDATA;
+
+static void CCONV writeToFile(PhidgetAccelerometerHandle ch, void * ctx, const double acceleration[3], double timestamp){
+
+//  PHIDGETDATA = fopen("./phidgetdata.txt", "w+");
+
+  char accelstr[] = "(";
+  char div[] = " | ";
+  const char fmt[] = "%f";
+  fputs(accelstr, PHIDGETDATA);
+
+  for (int i = 0; i < 3; i ++){
+    int size = snprintf(0,0,fmt,acceleration[i]);
+    char datapoint[size + 1];
+    sprintf(datapoint,fmt, acceleration[i]);
+  //  printf(datapoint);
+
+    fputs(datapoint, PHIDGETDATA);
+    fputs(",", PHIDGETDATA);
+
+    //strncat(accelstr, &div, sizeof(div))
+  }
+  int timesize = snprintf(0,0,fmt,timestamp);
+  char time[timesize + 1];
+  sprintf(time,fmt, timestamp);
+  fputs(time, PHIDGETDATA);
+  fputs(")\n", PHIDGETDATA);
+  //str_append(acceleration, sprintf(acceleration[0]));
+//  fputs("Acceleration: \t%lf  |  %lf  |  %lf\n", acceleration[0], acceleration[1], acceleration[2]);
+//	fputs("Timestamp: %lf\n", timestamp);
+
+}
+
+static void CCONV onAccelerationChange(PhidgetAccelerometerHandle ch, void * ctx, const double acceleration[3], double timestamp) {
+  writeToFile(ch, ctx, acceleration, timestamp);
+  printf("Acceleration: \t%lf  |  %lf  |  %lf\n", acceleration[0], acceleration[1], acceleration[2]);
+	printf("Timestamp: %lf\n", timestamp);
+	printf("----------\n");
+
+}
+
+
+int main() {
+	PhidgetAccelerometerHandle accelerometer0;
+
+	PhidgetAccelerometer_create(&accelerometer0);
+  PHIDGETDATA = fopen("./phidgetdata.txt", "w+");
+	PhidgetAccelerometer_setOnAccelerationChangeHandler(accelerometer0, onAccelerationChange, NULL);
+
+	Phidget_openWaitForAttachment((PhidgetHandle)accelerometer0, 5000);
+
+	//Wait until Enter has been pressed before exiting
+	getchar();
+  fclose(PHIDGETDATA);
+	Phidget_close((PhidgetHandle)accelerometer0);
+
+	PhidgetAccelerometer_delete(&accelerometer0);
+}
